c2bef6ddd2b7c157c67bf5222c9a9e81d69b6da3
John Barker <jdabarker@gmail.com>
2021-05-26 23:04:27 +0800

    refactor (testing) - implemented VIEW and ADD

--- index.js
+++ index.js
@@ -1,25 +1,136 @@
 require("dotenv").config();
+require("console.table");
+const inquirer = require("inquirer");
 const db = require("mysql2/promise");
-const ask = require("./dist/ask");
+let connection;
+
+async function welcome() {
+  await inquirer.prompt({
+    message: `
+.--------------------------------------.
+|              WELCOME TO              |
+|           EMPLOYEE TRACKER           |
+|         Press Enter To Begin         |
+'--------------------------------------'`,
+    type: "input",
+    name: "name",
+  });
+}
+
+async function pressEnter() {
+  await inquirer.prompt({
+    message: "Press ENTER to continue",
+    type: "input",
+    name: "name",
+  });
+}
+
+async function choose(message, choices) {
+  const answer = await inquirer.prompt({
+    message: message,
+    type: "list",
+    choices: choices,
+    name: "choice",
+  });
+  return answer.choice;
+}
+
+async function input(message) {
+  const answer = await inquirer.prompt({
+    message: message,
+    type: "input",
+    name: "input",
+  });
+  return answer.input;
+}
+
+async function budget() {
+  const departments = getAll("departments");
+  const department = await ask.department(departments);
+}
+
+async function getAll(tableName) {
+  return (await connection.query(`SELECT * FROM ${tableName}`))[0];
+}
+
+async function VIEW(tableName) {
+  const rows = await getAll(tableName);
+  console.log("\n", "\t", tableName.toUpperCase(), "\n");
+  console.table(rows);
+}
+
+function getCommaSeparatedColumnNames(columns) {
+  const columnNames = [];
+  for (const column of columns) {
+    if (column.Key != "PRI") {
+      columnNames.push(column.Field);
+    }
+  }
+  return columnNames;
+}
+
+async function getColumnValues(columns) {
+  let values = [];
+  for (const column of columns) {
+    if (column.Key != "PRI") {
+      values.push(await input(column.Field));
+    }
+  }
+  values = values.map(value =>
+    Number.isNaN(Number(value)) ? `"${value}"` : Number(value)
+  );
+  return values;
+}
+
+async function ADD(tableName) {
+  const columns = (await connection.query(`DESC ${tableName}`))[0];
+  const csColumnNames = getCommaSeparatedColumnNames(columns);
+  const values = await getColumnValues(columns);
+  const csv = values.join(",");
+  await connection.query(
+    `INSERT INTO ${tableName} (${csColumnNames}) VALUES (${values})`
+  );
+  await VIEW(tableName);
+}
+
+async function UPDATE(employee) {}
+
+async function DELETE(tableName) {}
 
 async function main() {
-    const connection = await db.createConnection({
-        host: process.env.HOST,
-        user: process.env.USER_NAME,
-        password: process.env.PASSWORD,
-        database: "employee_db",
-    });
-    let action = await ask.whatDoYouWantToDo();
-    while (action !== "Quit") {
-        switch (action) {
-            case "View all employees":
-                console.log("emp");
-                break;
-        }
-        action = await ask.continue();
-        action = await ask.whatDoYouWantToDo();
+  connection = await db.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER_NAME,
+    password: process.env.PASSWORD,
+    database: "employee_db",
+  });
+  console.clear();
+  await welcome();
+
+  const actions = ["VIEW", "ADD", "UPDATE", "DELETE", "QUIT"];
+  const tables = ["Departments", "Roles", "Employees"];
+
+  while (true) {
+    const action = await choose("Select action", actions);
+    if (action == "QUIT") {
+      break;
     }
-    connection.end();
+    const choices =
+      action == "VIEW"
+        ? ["Total Utilized Budget of A Department"].concat(tables)
+        : action == "UPDATE"
+        ? await getAll("employees")
+        : tables;
+    choices.push("CANCEL");
+    const choice = await choose(action, choices);
+    if (choice == "CANCEL") continue;
+    await eval(action)(choice.toLowerCase());
+    console.log("\n");
+    await pressEnter();
+    console.log("\n");
+  }
+
+  connection.end();
 }
 
 main();
--- seed.sql
+++ seed.sql
@@ -1,36 +1,36 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
-CREATE TABLE department(
+CREATE TABLE departments(
   id INT AUTO_INCREMENT,
   name VARCHAR(30),
   PRIMARY KEY (id)
 );
-CREATE TABLE role(
+CREATE TABLE roles(
   id INT AUTO_INCREMENT,
   title VARCHAR(30),
   salary DECIMAL,
   department_id INT,
   PRIMARY KEY (id),
-  FOREIGN KEY (department_id) REFERENCES department(id)
+  FOREIGN KEY (department_id) REFERENCES departments(id)
 );
-CREATE TABLE employee(
+CREATE TABLE employees(
   id INT AUTO_INCREMENT,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
   manager_id INT,
   PRIMARY KEY (id),
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
 INSERT INTO
-  department (name)
+  departments (name)
 VALUES
   ("kitchen"),
   ("bathroom");
 INSERT INTO
-  role (title, salary, department_id)
+  roles (title, salary, department_id)
 VALUES
   (
     "sales",
@@ -39,13 +39,13 @@ VALUES
       SELECT
         id
       FROM
-        department
+        departments
       WHERE
         name = "kitchen"
     )
   );
 INSERT INTO
-  role (title, salary, department_id)
+  roles (title, salary, department_id)
 VALUES
   (
     "warehouse",
@@ -54,13 +54,13 @@ VALUES
       SELECT
         id
       FROM
-        department
+        departments
       WHERE
         name = "kitchen"
     )
   );
 INSERT INTO
-  role (title, salary, department_id)
+  roles (title, salary, department_id)
 VALUES
   (
     "sales",
@@ -69,13 +69,13 @@ VALUES
       SELECT
         id
       FROM
-        department
+        departments
       WHERE
         name = "bathroom"
     )
   );
 INSERT INTO
-  role (title, salary, department_id)
+  roles (title, salary, department_id)
 VALUES
   (
     "warehouse",
@@ -84,13 +84,13 @@ VALUES
       SELECT
         id
       FROM
-        department
+        departments
       WHERE
         name = "bathroom"
     )
   );
 INSERT INTO
-  employee (first_name, last_name, role_id, manager_id)
+  employees (first_name, last_name, role_id, manager_id)
 VALUES
   (
     "James",
@@ -99,14 +99,14 @@ VALUES
       SELECT
         id
       FROM
-        role
+        roles
       WHERE
         title = "sales"
         AND department_id = (
           SELECT
             id
           FROM
-            department
+            departments
           WHERE
             name = "bathroom"
         )
@@ -114,7 +114,7 @@ VALUES
     NULL
   );
 INSERT INTO
-  employee (first_name, last_name, role_id, manager_id)
+  employees (first_name, last_name, role_id, manager_id)
 VALUES
   (
     "Sarah",
@@ -123,14 +123,14 @@ VALUES
       SELECT
         id
       FROM
-        role
+        roles
       WHERE
         title = "warehouse"
         AND department_id = (
           SELECT
             id
           FROM
-            department
+            departments
           WHERE
             name = "kitchen"
         )
@@ -143,8 +143,8 @@ VALUES
           SELECT
             *
           FROM
-            employee
-        ) AS employee
+            employees
+        ) AS employees
       WHERE
         first_name = "James"
         AND last_name = "Bond"
